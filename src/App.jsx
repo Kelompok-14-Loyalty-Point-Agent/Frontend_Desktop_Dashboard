a4a2f10cf381b92a162c5f3e0f8291dd430dec03
LuckDay21 <95541913+LuckDay21@users.noreply.github.com>
2023-05-23 11:31:57 +0700

    instal react-router-dom, add forgot password page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+// import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
-  return <div>welkom to mobail lejen</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<LandingPage />} /> */}
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
