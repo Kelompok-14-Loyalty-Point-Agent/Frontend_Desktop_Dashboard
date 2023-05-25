785ffd604df8474d62321b7f2f1231978a77e843
Nur Hanif <hanifmn11@gmail.com>
2023-05-25 15:58:30 +0700

    feat(signIn): add password toggle and change font size on the form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,7 +7,7 @@ const SignIn = () => {
 	return (
 		<>
 			<Navbar />
-			<Flex justify='center' align='center' height='96vh'>
+			<Flex justify='center' align='center' height='85vh'>
 				<SimpleGrid columns={2} spacing={120}>
 					<Image src='./Login-amico 1.svg' alt='Logo' py={50} align='center' />
 					<Form_SignIn />
