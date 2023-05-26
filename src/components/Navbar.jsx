13e621ec35f477b9bb9978348285cf62c7777e1f
Alfin Febrian <20k10020@student.unika.ac.id>
2023-05-26 10:18:09 +0700

    fix(landingPage): add link to landing and sign in page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,6 @@ import {
 } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { Link } from "react-router-dom";
-
 function Navbar() {
   const navigate = useNavigate();
   const handleClickSignIn = () => navigate("/signin");
