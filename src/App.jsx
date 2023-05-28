365c05a46c276ff21817c158b40df6de501ac09c
Nur Hanif <hanifmn11@gmail.com>
2023-05-28 18:00:56 +0700

    feat: done slicing UI on list customer transaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
 // import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ForgotPassword from "./pages/ForgotPassword";
-import Sidebar from "./pages/DashboardHome";
-import DataCreditStock from "./pages/DataCreditStock";
-import CreateNewPassword from "./pages/CreateNewPassword";
-import LandingPage from "./pages/LandingPage";
-import VerifyEmail from "./pages/VerifyEmail";
-import SignIn from "./pages/SignIn";
-import CustomerData from "./pages/CustomerData";
-import DashboardProfile from "./pages/DashboardProfile";
-import CashOutStock from "./pages/CashOutStock";
-import AddStock from "./pages/AddStock";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ForgotPassword from './pages/ForgotPassword';
+import Sidebar from './pages/DashboardHome';
+import DataCreditStock from './pages/DataCreditStock';
+import CreateNewPassword from './pages/CreateNewPassword';
+import LandingPage from './pages/LandingPage';
+import VerifyEmail from './pages/VerifyEmail';
+import SignIn from './pages/SignIn';
+import CustomerData from './pages/CustomerData';
+import DashboardProfile from './pages/DashboardProfile';
+import CashOutStock from './pages/CashOutStock';
+import AddStock from './pages/AddStock';
+import CustomerTransaction from './pages/CustomerTransaction';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/forgot" element={<ForgotPassword />} />
-          <Route path="/verify" element={<VerifyEmail />} />
-          <Route path="/newpassword" element={<CreateNewPassword />} />
-          <Route path="/dashboard" element={<Sidebar />} />
-          <Route path="/profile" element={<DashboardProfile />} />
-          <Route path="/customers" element={<CustomerData />} />
-          <Route path="/transactions" />
-          <Route path="/stocks" element={<DataCreditStock />} />
-          <Route path="/stocks/add" element={<AddStock />} />
-          <Route path="/cashout" element={<CashOutStock />} />
-        </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path='/' element={<LandingPage />} />
+					<Route path='/signin' element={<SignIn />} />
+					<Route path='/forgot' element={<ForgotPassword />} />
+					<Route path='/verify' element={<VerifyEmail />} />
+					<Route path='/newpassword' element={<CreateNewPassword />} />
+					<Route path='/dashboard' element={<Sidebar />} />
+					<Route path='/profile' element={<DashboardProfile />} />
+					<Route path='/customers' element={<CustomerData />} />
+					<Route path='/transactions' element={<CustomerTransaction />} />
+					<Route path='/stocks' element={<DataCreditStock />} />
+					<Route path='/stocks/add' element={<AddStock />} />
+					<Route path='/cashout' element={<CashOutStock />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
