e696d0d0156b7931e77f614452d7e4a1d169479a
Nur Hanif <hanifmn11@gmail.com>
2023-06-12 10:25:49 +0700

    feat: add redux on signin

---
+++ src/config/redux/store.jsx
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunkMiddleware from 'redux-thunk';
+import reducer from './reducer';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: 'signIn',
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: [thunkMiddleware],
+});
+
+export const persiststore = persistStore(store);
+
+export default store;
